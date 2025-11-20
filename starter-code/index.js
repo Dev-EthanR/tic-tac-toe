173c11b8e5dd6d6af27408fd4415e293fc4c2672
Ethan Rechichi <ethanlegend7@gmail.com>
2025-11-20 18:47:39 +1100

    init

---
+++ starter-code/index.js
@@ -0,0 +1,5 @@
+/* // TODO 
+ Play the game either solo vs the computer or multiplayer against another person
+ Save the game state in the browser so that it’s preserved if the player refreshes their browser
+ Instead of having the computer randomly make their moves, try making it clever so it’s proactive in blocking your moves and trying to win
+*/
