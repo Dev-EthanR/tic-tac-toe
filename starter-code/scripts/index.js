9ce448efcb9b9ba340446ed7a0cabb05da1fd8ee
Ethan Rechichi <ethanlegend7@gmail.com>
2025-11-21 16:14:24 +1100

    menu logic created

--- starter-code/scripts/index.js
+++ starter-code/scripts/index.js
@@ -3,3 +3,16 @@
  Save the game state in the browser so that it’s preserved if the player refreshes their browser
  Instead of having the computer randomly make their moves, try making it clever so it’s proactive in blocking your moves and trying to win
 */
+
+import { setMenu } from './menu.js';
+
+const appState = {
+    mark: null,
+    mode: null,
+}
+
+console.log(appState)
+document.querySelectorAll('.menu-btn').forEach(btn => btn.addEventListener('click', (e) => {
+     Object.assign(appState, setMenu(e));
+     console.log(appState)
+}));
---
+++ starter-code/scripts/menu.js
@@ -0,0 +1,14 @@
+const checkbox = document.getElementById('switchCheckBox');
+
+function sliderOption() {
+    document.getElementById('sliderNortImg').classList.toggle('menu-img-active', checkbox.checked);
+    document.getElementById('sliderCrossImg').classList.toggle('menu-img-active', !checkbox.checked);
+}
+
+
+export function setMenu(e) {
+    const mark = checkbox.checked ? "o" : "x";
+    const mode = e.target.dataset.mode;
+    return {mark, mode}
+}
+checkbox.addEventListener('change', sliderOption)
